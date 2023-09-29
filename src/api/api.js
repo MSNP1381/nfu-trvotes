2182ea2b558b4307eab30d3155ef663a7d268c03
MSNP1381 <mohamadnematpoor@gmail.com>
2023-09-29 10:38:19 +0330

    delete js

--- src/api/api.js
+++
@@ -1,5 +0,0 @@
-import {Ax} from axios;
-export const base_url="https://localhost:7027"
-function get_token(username,password){
-    return axios.post(`${base_url}/api/Authenticate/login`,{username,password})
-}
